46bd46243278f96517a0746c35b5784cfcccf6e8
KleberTrugilo <klebertrugilo01@gmail.com>
2022-10-17 17:24:16 -0300

    103: implementa a validação da existência de uma seção.

--- paginas/creator.php
+++ paginas/creator.php
@@ -31,123 +31,147 @@ if (empty($id)) {
     </div>
 
     <h2 class="text-center">Comics</h2>
-    <div class="row">
-        <?php
+    <div class="row text-center">
+    <?php
         $arquivo = "{$url}/creators/{$id}/comics?{$apiKey}";
         $dados = file_get_contents($arquivo);
         $dados = json_decode($dados);
 
-        foreach ($dados->data->results as $comics) {
-            $id = $comics->id;
-            $title = $comics->title;
-            $path = $comics->thumbnail->path;
-            $extension = $comics->thumbnail->extension;
-            $image = $path . "." . $extension;
-        ?>
-            <div class="col-12 col-md-3">
-                <div class="card">
-                    <div class="card-body text-center">
-                        <img src="<?= $image ?>" alt="<?= $title ?>" class="card-img-top w-100 h-75">
-                        <p class="card-title"><?= $title ?></p>
-                        <p>
-                            <a href="comic/<?= $id ?>" class="btn btn-warning">See more</a>
-                        </p>
+        if(empty($dados->data->results)){
+            ?>
+                <p>Comics not available.</p>
+            <?php
+        } else {
+            foreach ($dados->data->results as $comics) {
+                $id = $comics->id;
+                $title = $comics->title;
+                $path = $comics->thumbnail->path;
+                $extension = $comics->thumbnail->extension;
+                $image = $path . "." . $extension;
+            ?>
+                <div class="col-12 col-md-3">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <img src="<?= $image ?>" alt="<?= $title ?>" class="card-img-top w-100 h-75">
+                            <p class="card-title"><?= $title ?></p>
+                            <p>
+                                <a href="comic/<?= $id ?>" class="btn btn-warning">See more</a>
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        <?php
+            <?php
+            }
         }
-        ?>
+            ?>
     </div>
 
     <h2 class="text-center">Series</h2>
-    <div class="row">
+    <div class="row text-center">
         <?php
         $arquivo = "{$url}/creators/{$id}/series?{$apiKey}";
         $dados = file_get_contents($arquivo);
         $dados = json_decode($dados);
 
-        foreach ($dados->data->results as $series) {
-            $id = $series->id;
-            $title = $series->title;
-            $path = $series->thumbnail->path;
-            $extension = $series->thumbnail->extension;
-            $image = $path . "." . $extension;
-        ?>
-            <div class="col-12 col-md-3">
-                <div class="card">
-                    <div class="card-body text-center">
-                        <img src="<?= $image ?>" alt="<?= $title ?>" class="card-img-top w-100 h-75">
-                        <p class="card-title"><?= $title ?></p>
-                        <p>
-                            <a href="serie/<?= $id ?>" class="btn btn-warning">See more</a>
-                        </p>
+        if(empty($dados->data->results)) {
+            ?>
+                <p>Series not available.</p>
+            <?php
+        } else {
+            foreach ($dados->data->results as $series) {
+                $id = $series->id;
+                $title = $series->title;
+                $path = $series->thumbnail->path;
+                $extension = $series->thumbnail->extension;
+                $image = $path . "." . $extension;
+            ?>
+                <div class="col-12 col-md-3">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <img src="<?= $image ?>" alt="<?= $title ?>" class="card-img-top w-100 h-75">
+                            <p class="card-title"><?= $title ?></p>
+                            <p>
+                                <a href="serie/<?= $id ?>" class="btn btn-warning">See more</a>
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        <?php
+            <?php
+            }
         }
-        ?>
+            ?>
     </div>
 
     <h2 class="text-center">Stories</h2>
-    <div class="row">
+    <div class="row text-center">
         <?php
         $arquivo = "{$url}/creators/{$id}/stories?{$apiKey}";
         $dados = file_get_contents($arquivo);
         $dados = json_decode($dados);
 
-        foreach ($dados->data->results as $stories) {
-            $id = $stories->id;
-            $title = $stories->title;
-            $path = $stories->thumbnail->path ?? null;
-            $extension = $stories->thumbnail->extension ?? null;
-            $image = $path . "." . $extension;
-        ?>
-            <div class="col-12 col-md-3">
-                <div class="card">
-                    <div class="card-body text-center">
-                        <img src="<?= $image ?>" alt="<?= $title ?>" class="card-img-top w-100 h-75">
-                        <p class="card-title"><?= $title ?></p>
-                        <p>
-                            <a href="storie/<?= $id ?>" class="btn btn-warning">See more</a>
-                        </p>
+        if (empty($dados->data->results)){
+            ?>
+                <p>Stories not available.</p>
+            <?php
+        } else {
+            foreach ($dados->data->results as $stories) {
+                $id = $stories->id;
+                $title = $stories->title;
+                $path = $stories->thumbnail->path ?? null;
+                $extension = $stories->thumbnail->extension ?? null;
+                $image = $path . "." . $extension;
+            ?>
+                <div class="col-12 col-md-3">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <img src="<?= $image ?>" alt="<?= $title ?>" class="card-img-top w-100 h-75">
+                            <p class="card-title"><?= $title ?></p>
+                            <p>
+                                <a href="storie/<?= $id ?>" class="btn btn-warning">See more</a>
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        <?php
+            <?php
+            }
         }
-        ?>
+            ?>
     </div>
 
     <h2 class="text-center">Events</h2>
-    <div class="row">
+    <div class="row text-center">
         <?php
         $arquivo = "{$url}/creators/{$id}/events?{$apiKey}";
         $dados = file_get_contents($arquivo);
         $dados = json_decode($dados);
 
-        foreach ($dados->data->results as $events) {
-            $id = $events->id;
-            $title = $events->title;
-            $path = $events->thumbnail->path;
-            $extension = $events->thumbnail->extension;
-            $image = $path . "." . $extension;
-        ?>
-            <div class="col-12 col-md-3">
-                <div class="card">
-                    <div class="card-body text-center">
-                        <img src="<?= $image ?>" alt="<?= $title ?>" class="card-img-top w-100 h-75">
-                        <p class="card-title"><?= $title ?></p>
-                        <p>
-                            <a href="event/<?= $id ?>" class="btn btn-warning">See more</a>
-                        </p>
+        if (empty($dados->data->results)) {
+            ?>
+                <p>Eventes not available</p>
+            <?php
+        } else {
+            foreach ($dados->data->results as $events) {
+                $id = $events->id;
+                $title = $events->title;
+                $path = $events->thumbnail->path;
+                $extension = $events->thumbnail->extension;
+                $image = $path . "." . $extension;
+            ?>
+                <div class="col-12 col-md-3">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <img src="<?= $image ?>" alt="<?= $title ?>" class="card-img-top w-100 h-75">
+                            <p class="card-title"><?= $title ?></p>
+                            <p>
+                                <a href="event/<?= $id ?>" class="btn btn-warning">See more</a>
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        <?php
+            <?php
+            }
         }
-        ?>
+            ?>
     </div>
 <?php
 }
