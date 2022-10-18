3ce4a2c9c3a5a861582139b612dfccc397599d1d
KleberTrugilo <klebertrugilo01@gmail.com>
2022-10-17 21:25:09 -0300

    102: Refatoração da validação de existência de seção e título comic na página

--- paginas/comic.php
+++ paginas/comic.php
@@ -20,18 +20,21 @@ if (empty($id)) {
     $extension = $comic->thumbnail->extension;
     $image = $path . "/portrait_uncanny." . $extension;
 ?>
+    <h1 class="text-center my-5">Comic</h1>
     <div class="card">
         <div class="row">
             <div class="col-12 col-md-3">
                 <img src="<?= $image ?>" alt="<?= $title ?>" class="w-100">
             </div>
             <div class="col-12 col-md-9">
-                <h1 class="titulo text-center"><?= $title ?></h1>
+                <h3 class="titulo text-center"><?= $title ?></h3>
                 <p>
                     <?php 
                         if(empty($description)) {
                             ?>
-                                <p>Description not available.</p>
+                                <p>Description not available. For more information 
+                                    <a href="https://www.marvel.com/" target="blank">click here</a>
+                                </p>
                             <?php
                         } else {
                             ?>
@@ -45,98 +48,90 @@ if (empty($id)) {
         </div>
     </div>
 
-    <div class="container text-center my-3">
-        <h2 class="text-center">Characters</h2>
-            <?php
-                $arquivo = "{$url}/comics/{$id}/characters?{$apiKey}";
-                $dados =  file_get_contents($arquivo);
-                $dados = json_decode($dados);
-
-                if(empty($dados->data->results)) {
-                    ?>
-                        <p>Characters not available.</p>
-                    <?php
-                } else {
-                    ?>
-                        <div id="carouselControls" class="carousel" data-bs-ride="carousel">
-                            <div class="carousel-inner d-flex d-inline-block">
-                                <?php
-                                    foreach ($dados->data->results as $characters) {
-                                        $path = $characters->thumbnail->path;
-                                        $extension = $characters->thumbnail->extension;
-                                        $image = $path . "/portrait_uncanny." . $extension;
-                                        $name = $characters->name;
-                                        $id = $characters->id;
-                                ?>
-                                    <div class="col-12 col-md-3 px-1">
-                                        <div class="card card-body h-100 text-center">
-                                            <img class="img-fluid w-100 h-75" src="<?= $image ?>" alt="<?= $name ?>">
-                                            <p class="titulo">
-                                                <strong>
-                                                    <?= $name ?>
-                                                </strong>
-                                            </p>
-                                            <p>
-                                                <a href="character/<?= $id ?>" class="btn btn-warning"> See more</a>
-                                            </p>
-                                        </div>
-                                    </div>
-                                <?php
-                                    }
-                                ?>
-                            </div>
-                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselControls" data-bs-slide="prev" onclick="sliderScrollLeft()">
-                                <span class="carousel-control-prev-icon bg-dark border border-dark" aria-hidden="true"></span>
-                                <span class="visually-hidden">Previous</span>
-                            </button>
-                            <button class="carousel-control-next" type="button" data-bs-target="#carouselControls" data-bs-slide="next" onclick="sliderScrollRight()">
-                                <span class="carousel-control-next-icon bg-dark border border-dark" aria-hidden="true"></span>
-                                <span class="visually-hidden">Next</span>
-                            </button>
-                        </div>
-                    <?php
-                }
+    <?php
+        $arquivo = "{$url}/comics/{$id}/characters?{$apiKey}";
+        $dados =  file_get_contents($arquivo);
+        $dados = json_decode($dados);
+        
+        if(!empty($dados->data->results)) {
             ?>
-    </div>
-
-    <h2 class="text-center">Creators</h2>
-    <div class="row">
-        <?php
-            $arquivo = "{$url}/comics/{$id}/creators?{$apiKey}";
-            $dados = file_get_contents($arquivo);
-            $dados = json_decode($dados);
-
-            if(empty($dados->data->results)){
-                ?>
-                    <p class="text-center">Creators not available.</p>
-                <?php
-            } else {
-                foreach ($dados->data->results as $creators) {
-                    $path = $creators->thumbnail->path;
-                    $extension = $creators->thumbnail->extension;
-                    $image = $path . "/portrait_uncanny." . $extension;
-                    $id = $creators->id;
-                    $fullName = $creators->fullName;
-    
-                    ?>
-                        <div class="col-12 col-md-3">
-                            <div class="card text-center">
-                                <img src="<?= $image?>" alt="<?= $fullName?>" class="w-100">
-                                <p class="titulo">
-                                    <strong>
-                                        <?=$fullName?>
-                                    </strong>
-                                    <p>
-                                        <a href="creators/<?=$id?>" class="btn btn-warning"> See More</a>
-                                    </p>
-                                </p>
-                            </div>
+                <h2 class="text-center">Characters</h2>
+                <div class="container text-center my-3">
+                    <div id="carouselControls" class="carousel" data-bs-ride="carousel">
+                        <div class="carousel-inner d-flex d-inline-block">
+                            <?php
+                                foreach ($dados->data->results as $characters) {
+                                    $path = $characters->thumbnail->path;
+                                    $extension = $characters->thumbnail->extension;
+                                    $image = $path . "/portrait_uncanny." . $extension;
+                                    $name = $characters->name;
+                                    $id = $characters->id;
+                            ?>
+                                <div class="col-12 col-md-3 px-1">
+                                    <div class="card card-body h-100 text-center">
+                                        <img class="img-fluid w-100 h-75" src="<?= $image ?>" alt="<?= $name ?>">
+                                        <p class="titulo">
+                                            <strong>
+                                                <?= $name ?>
+                                            </strong>
+                                        </p>
+                                        <p>
+                                            <a href="character/<?= $id ?>" class="btn btn-warning"> See more</a>
+                                        </p>
+                                    </div>
+                                </div>
+                            <?php
+                                }
+                            ?>
                         </div>
-                    <?php
-                }
-            }
-        ?>
-    </div>
-<?php
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselControls" data-bs-slide="prev" onclick="sliderScrollLeft()">
+                            <span class="carousel-control-prev-icon bg-dark border border-dark" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselControls" data-bs-slide="next" onclick="sliderScrollRight()">
+                            <span class="carousel-control-next-icon bg-dark border border-dark" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
+            <?php
+        }
+        
+        $arquivo = "{$url}/comics/{$id}/creators?{$apiKey}";
+        $dados = file_get_contents($arquivo);
+        $dados = json_decode($dados);
+        
+        if(!empty($dados->data->results)){
+            ?>
+                <h2 class="text-center">Creators</h2>
+                <div class="row">
+            <?php
+                    foreach ($dados->data->results as $creators) {
+                        $path = $creators->thumbnail->path;
+                        $extension = $creators->thumbnail->extension;
+                        $image = $path . "/portrait_uncanny." . $extension;
+                        $id = $creators->id;
+                        $fullName = $creators->fullName;
+
+                        ?>
+                            <div class="col-12 col-md-3">
+                                <div class="card text-center">
+                                    <img src="<?= $image?>" alt="<?= $fullName?>" class="w-100">
+                                    <p class="titulo">
+                                        <strong>
+                                            <?=$fullName?>
+                                        </strong>
+                                        <p>
+                                            <a href="creators/<?=$id?>" class="btn btn-warning"> See More</a>
+                                        </p>
+                                    </p>
+                                </div>
+                            </div>
+                            <?php
+                    }
+                    ?>
+                </div>
+            <?php
+        }
 }
 ?>
