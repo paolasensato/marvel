0bb4624c931c83b38643ab02b075c1f312a47d50
paolasensato <sensatopaola@gmail.com>
2022-11-13 23:35:14 -0300

    Altera page de erro

--- paginas/erro.php
+++ paginas/erro.php
@@ -1,6 +1,20 @@
-<h1 class="text-center head-font">
-    Erro 404
-</h1>
-<p class="text-center">
-    <img src="images/erro.webp" alt="404" class="w-50">
-</p>
+<div class="banner-erro">
+    <div class="erro">
+        <div class="page-not-found">
+            <h1><strong>404</strong></h1>
+            <h2>PAGE NOT FOUND</h3>
+        </div>
+        <div class="text-erro">
+            <h4>Oh Snap!</h4>
+            <p>Don't be angry, but something went wrong here. <br> The page you are looking for may have been blown apart by the snap of Thanus..</p>
+        </div>
+        <a href="/marvel/home">
+            <button class="btn btn-outline-light disabled" role="button" aria-pressed="false" style="--bs-btn-disabled-opacity: none;">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+                        <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"></path>
+                    </svg>
+                    Home
+            </button>
+        </a>
+    </div>
+</div>
